ce94defd7721d3b57e2698283dada410a01584c0
Greeshma D J <greeshmadj1@gmail.com>
2025-08-13 21:34:23 +0530

    Whitefield Vihar initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+  const aboutText = document.querySelector('.about-text');
+  aboutText.style.opacity = '1';
+  aboutText.style.transform = 'translateY(0)';
+});
